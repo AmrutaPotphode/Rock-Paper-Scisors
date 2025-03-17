43be3f90fe0830b796094f0c3b693d42aa64f074
AmrutaPotphode <amrutapotphode001@gmail.com>
2025-03-17 13:10:20 +0530

    added index

---
+++ script.js
@@ -0,0 +1,10 @@
+const computerchoicecanbe = ["Rock", "Paper", "Scisor"];
+
+const computerchoice = Math.floor(Math.random() * 3);
+console.log(computerchoicecanbe[computerchoice]);
+
+function playGame(userChoice){
+    console.log("user's choice is :", userChoice);
+}
+
+console.log("user's choice is :", userChoice);
